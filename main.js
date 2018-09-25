f1d1581ba797e892969a16018c4f8f4ab40f4be7
Leonardo Felix Gajardo <leonardo@gajardo.com.br>
2018-09-25 18:44:51 -0300

    Add files via upload
    adding initial files for simulation

---
+++ main.js
@@ -0,0 +1,15 @@
+const Random = require("random-js");
+//var random = new Random(Random.engines.mt19937().autoSeed());
+//var value = random.integer(1, 100);const random = require('random-js')();
+
+function main(){
+
+    
+
+}
+
+function createClient(position){
+    return {
+       position: position 
+    }
+}
